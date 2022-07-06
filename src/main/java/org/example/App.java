a3b54c248e12763a12cd5228058be198df0f11d4
Ludo <lraulin843@gmail.com>
2022-07-06 11:53:51 +0200

    added entitymanagersingleton

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,10 @@
 package org.example;
 
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
     }
 }
---
+++ src/main/java/org/example/hibernate/EntityManagerSingleton.java
@@ -0,0 +1,20 @@
+package org.example.hibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerSingleton {
+
+    private static EntityManager entityManager;
+
+    public static EntityManager getEntityManager(){
+
+        if (entityManager == null){
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("crm");
+            EntityManager entityManager = emf.createEntityManager();
+        }
+
+        return entityManager;
+    }
+}
